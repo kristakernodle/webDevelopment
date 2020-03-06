7c73e79eeb7db7edd30d1da8239996189ab72ab7
Krista Kernodle <kristakernodle@gmail.com>
2020-03-06 18:49:16 -0500

    Updated urls file to tell when to serve view.home function

--- django/firstProject/firstProject/firstProject/urls.py
+++ django/firstProject/firstProject/firstProject/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from boards import views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r'^$', views.home, name='home'),
 ]
