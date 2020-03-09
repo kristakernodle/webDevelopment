54b1fb6531d0e0d194c49823770c7a0ddf8c81c7
kristakernodle@gmail.com <kristakernodle@gmail.com>
2020-03-09 18:40:16 -0400

    Code written and test passes

--- django/hangmanAPI/mainApp/models.py
+++ django/hangmanAPI/mainApp/models.py
@@ -1,5 +1,18 @@
 from django.db import models
+import requests
+import hangmanAPI.settings as settings
 
 
 class Word(models.Model):
     solution = models.CharField(max_length=10)
+
+    def get_random_word(self):
+        url = "https://wordsapiv1.p.rapidapi.com/words/"
+        querystring = {"random": "true", "lettersMin": 3, "lettersMax": 10}
+        headers = {
+            'x-rapidapi-host': "wordsapiv1.p.rapidapi.com",
+            'x-rapidapi-key': settings.SECRET_KEY
+        }
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        entireResponse = response.json()
+        return entireResponse['word']
--- django/hangmanAPI/mainApp/tests.py
+++ django/hangmanAPI/mainApp/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from mainApp.models import Word
 
-# Create your tests here.
+class WordTestCase(TestCase):
+    def test_get_random_word(self):
+        randomWord = Word
+        randomWord = Word.get_random_word(randomWord)
+        self.assertTrue(randomWord, type(randomWord) == str)
+
