cd66fcb55e98a28e6b0399f79595b0aae13c5d40
kristakernodle@gmail.com <kristakernodle@gmail.com>
2020-03-09 19:57:23 -0400

    First Game class test

--- django/hangmanAPI/mainApp/tests.py
+++ django/hangmanAPI/mainApp/tests.py
@@ -2,6 +2,12 @@ from django.test import TestCase
 import mainApp.models as mds
 
 
+class GameTestCase(TestCase):
+    def test_game_state(self):
+        game1 = mds.Game()
+        self.assertTrue(game1.state, False)
+
+
 class WordTestCase(TestCase):
     def test_get_random_word(self):
         wordPair = mds.Word()
