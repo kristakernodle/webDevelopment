4b321bf8a67e98cc2c4fcdeaa0cda3986d7ebeea
kristakernodle@gmail.com <kristakernodle@gmail.com>
2020-03-09 19:21:23 -0400

    Refactoring test_get_random_word

--- django/hangmanAPI/mainApp/tests.py
+++ django/hangmanAPI/mainApp/tests.py
@@ -1,10 +1,10 @@
 from django.test import TestCase
 import mainApp.models as mds
 
+
 class WordTestCase(TestCase):
     def test_get_random_word(self):
-        randomWord = mds.Word
-        randomWord.get_random_word(randomWord)
+        randomWord = mds.Word()
+        randomWord.get_random_word()
         self.assertTrue(type(randomWord.solution), str)
         self.assertTrue(randomWord.solution.isalpha(), True)
-
